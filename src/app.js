e7a8685a1b56fc1dc8e0d376c9b74d08854c7f0b
dev126712 <solonoobhacker6767@gmail.com>
2025-10-03 02:16:30 +0000

    implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-   res.status(200).send('Hello from Acquisition')
+  res.status(200).send('Hello from Acquisition');
 });
 
 export default app;
-
-
